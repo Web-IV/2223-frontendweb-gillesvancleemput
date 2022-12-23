794424911e8daed51e048b449cffdac1ed3f1405
Gilles Van Cleemput <gilles.vancleemput@student.hogent.com>
2022-12-23 19:18:55 +0100

    dossier aangepast

--- sinsin/src/components/contactMail.jsx
+++ sinsin/src/components/contactMail.jsx
@@ -1,16 +1,13 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import emailjs from "@emailjs/browser";
-import { useNavigate } from "react-router-dom";
 
 function Contact() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  const Navigate = useNavigate();
 
   const form = useRef();
 
